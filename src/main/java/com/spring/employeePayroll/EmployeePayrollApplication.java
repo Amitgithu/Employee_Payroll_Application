c60127da912989065a45bbb9979c485833717d1d
Amit Kumar <amit909563@gmail.com>
2025-03-10 19:10:55 +0530

    Added: Dotenv Setup Code to load env variables

--- src/main/java/com/spring/employeePayroll/EmployeePayrollApplication.java
+++ src/main/java/com/spring/employeePayroll/EmployeePayrollApplication.java
@@ -2,11 +2,16 @@ package com.spring.employeePayroll;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import io.github.cdimascio.dotenv.Dotenv;
 
 @SpringBootApplication
 public class EmployeePayrollApplication {
 
 	public static void main(String[] args) {
+		Dotenv.configure()
+				.systemProperties()
+				.load();
+
 		SpringApplication.run(EmployeePayrollApplication.class, args);
 	}
 
